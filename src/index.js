688d34c736fb41e022db0b15f4117318ee8df2a1
Aleksandr <abyzovaleksandr5@gmail.com>
2023-02-27 15:52:17 +0500

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+  const some = "**********";
+  const arrWordCode = expr.split(some);
+  const arrWordDecode = arrWordCode.map((j) => {
+    let newArrDecode = [];
+    for (let i = 0; i < j.length; i += 10) {
+      const decodeValue = j
+        .slice(i, i + 10)
+        .replace(/10/g, ".")
+        .replace(/11/g, "-")
+        .replace(/00/g, "");
+        newArrDecode.push(MORSE_TABLE[decodeValue]);
+    }
+    return newArrDecode.join("");
+  });
+  return arrWordDecode.join(" ");
 }
+
 
 module.exports = {
     decode
 }
+
